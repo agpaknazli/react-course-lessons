75bc2101abbe062b826973ffae4f9cf19c42bf53
agpaknazli <agpaknazli19@gmail.com>
2024-08-21 20:37:00 +0200

    Header.jsx

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,67 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
 
 function App() {
-let sum=1000;
-const surname="Elif";
-let vize1=30;
-let vize2=80;
-let obst=[
-  "elma",
-  "armut",
-  "muz",
-  "çilek"
+// let sum=1000;
+// const surname="Elif";
+// let vize1=30;
+// let vize2=80;
+// let obst=[
+//   "elma",
+//   "armut",
+//   "muz",
+//   "çilek"
 
-]
+// ]
 
 
   return (
 
-<body style={{
-  background:"pink"
-}}>
- <div>
-<p> ürün toplamı:{sum} </p>
-<p>kullanıcı isimi:{surname}</p>
-<p>öğrenci ortalaması:{(vize1+vize2)/2}</p>
+<>
 
-<p>
-{
-obst.map((meyve,index)=>(
-  <p style={{
-    color:"red",
-    backgroundColor:"yellowgreen",
-    padding:"1rem",
-    margin:"3rem"
+<Header/>
 
 
-  }} >
-    {meyve}
-  </p>
-
-)
-)}
-</p>
+</>
 
 
+// <body style={{
+//   background:"pink"
+// }}>
+//  <div>
+// <p> ürün toplamı:{sum} </p>
+// <p>kullanıcı isimi:{surname}</p>
+// <p>öğrenci ortalaması:{(vize1+vize2)/2}</p>
 
-   </div>
+// <p>
+// {
+// obst.map((meyve,index)=>(
+//   <p style={{
+//     color:"red",
+//     backgroundColor:"yellowgreen",
+//     padding:"1rem",
+//     margin:"3rem"
 
-   <div style={{backgroundColor:"yellow",
-    width:"100%",
-    height:"50px",
-   }}> 
-   sonuc {(vize1+vize2)/2>= 50 ? "sınavdan geçtiniz" : "Sınavdan kaldınız"}
-   </div>
+
+//   }} >
+//     {meyve}
+//   </p>
+
+// )
+// )}
+// </p>
+
+
+
+//    </div>
+
+//    <div style={{backgroundColor:"yellow",
+//     width:"100%",
+//     height:"50px",
+//    }}> 
+//    sonuc {(vize1+vize2)/2>= 50 ? "sınavdan geçtiniz" : "Sınavdan kaldınız"}
+//    </div>
    
-</body>
+// </body>
 
+//     // <div className="App">
+//     //   <h1>Cohort 17</h1>
+//     // </div>
  
 
-
-    // <div className="App">
-    //   <h1>Cohort 17</h1>
-    // </div>
   );
 }
 
---
+++ lesson1/src/Header.jsx
@@ -0,0 +1,21 @@
+
+
+
+const Header=()=>{
+
+return(
+<header>
+
+<h1>Hello fs-17 </h1>
+
+</header>
+
+
+
+);
+
+
+};
+
+export default Header;
+
