f588c7e39d32d3a1228c61461a04d904f1fc7fe9
agpaknazli <agpaknazli19@gmail.com>
2024-08-26 20:33:32 +0200

    button

--- lesson4/src/components/1-events/Event.jsx
+++ lesson4/src/components/1-events/Event.jsx
@@ -1,28 +1,79 @@
 const Event = () => {
-  //? Event handler fonksiyon tanımlaması
-  const handleTıkla = (event) => {
-    alert("tıklandı");
-    //* event objesi üzerinden elementin özelliklerine ulaşabiliriz
-    console.log(event);
-    console.log("id", event.target.id);
-    console.log("name", event.target.name);
+  <<<<<<< HEAD
+  =======
+    //? local değişken
+    let message = "Güle güle FS-17";
+  >>>>>>> ab95a3e4141653775d7e5b61ff95b07c1ef48d29
+    //? Event handler fonksiyon tanımlaması
+    const handleTıkla = (event) => {
+      alert("tıklandı");
+      //* event objesi üzerinden elementin özelliklerine ulaşabiliriz
+      console.log(event);
+      console.log("id", event.target.id);
+      console.log("name", event.target.name);
+    };
+  
+  <<<<<<< HEAD
+  =======
+    function handleÇıkış(msg) {
+      console.log(msg);
+    }
+  
+    const handleDeğiştir = () => {
+      message = "Merhaba React Dostları";
+      console.log(message);
+      //! React'da DOM'a direk müdahale tercih edilmez
+      // document.querySelector("p").textContent = message;
+    };
+  
+    //? Bir componentin return kismi 3 farkli yontem ile yeniden render edilir.
+    //? 1- Herhangi bir state degisirse
+    //? 2- Props degisirse
+    //? 3- Refresh force edilirse (tercih edilen bir durum olmaz)
+  >>>>>>> ab95a3e4141653775d7e5b61ff95b07c1ef48d29
+    return (
+      <div>
+        <h2>Events</h2>
+        <button
+          onClick={handleTıkla}
+          id="myButton"
+          name="tıkla"
+          className="btn btn-primary"
+        >
+          Tıkla
+        </button>
+        <button onClick={() => alert("Silindi")} className="btn btn-danger">
+          Sil
+        </button>
+  <<<<<<< HEAD
+  =======
+  
+        {/* //* Eger bir event fonksiyonunun parametresi olmasi gerekiyorsa bu fonksiyon bir arrow fonks. tarafindan  cagrilmalidir. Aksi takdirde event fonksiyonu, event gerceklesmeden cagirilir */}
+        <button onClick={() => handleÇıkış(message)} className="btn btn-success">
+          Çıkış
+        </button>
+        <button onClick={handleDeğiştir} className="btn btn-warning">
+          Değiştir
+        </button>
+        <p>{message} </p>
+  >>>>>>> ab95a3e4141653775d7e5b61ff95b07c1ef48d29
+      </div>
+    );
   };
-
-  return (
-    <div>
-      <h2>Events</h2>
-      <button
-        onClick={handleTıkla}
-        id="myButton"
-        name="tıkla"
-        className="btn btn-primary"
-      >
-        Tıkla
-      </button>
-      <button onClick={() => alert("Silindi")} className="btn btn-danger">
-        Sil
-      </button>
-    </div>
-  );
-};
-export default Event;
+  export default Event;
+  
+  //! message console'da guncellendigini ancak DOM'da guncellenmedigini gorduk.
+  //* Bunun sebebi REACT'in aksi belirtilmedigi surece elementleri
+  //* static olarak kabul etmesinden kaynaklanir.
+  
+  //* React bunu DOM islemlerini minimize etmek icin yapmaktadir.
+  //* REACT'a hangi elementleri interaktif oldugu belirtilmelidir.
+  //! React'a elementlerin interaktif oldugunu belirtmek icin state'ler kullanilir.
+  //! State, elementlerin degisiklik durumlarini tutan nesnelerdir.
+  //? ReactJs'de state'leri kullanmak icin  2 ayri Component yapisi bulunmaktadir.
+  //? 1. Hooks (Functional Components).
+  //? 2. Statefull Classes (Class Components).
+  
+  //? Biz su ana kadar uygulamalarimizda Fonksiyonel Component'leri kullandik.
+  //? Yaygin kullanim Fonksiyonel Component'lerdir.
+  
--- lesson4/src/index.js
+++ lesson4/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode> clg iki kez bastırır bu çalışıyor durumdayken
     <App />
   </React.StrictMode>
 );
