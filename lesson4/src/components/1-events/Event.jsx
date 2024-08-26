eb896414c15fe665b21ef38d2eeec1f41ab54624
agpaknazli <agpaknazli19@gmail.com>
2024-08-26 19:56:26 +0200

    onClick

--- lesson4/src/components/1-events/Event.jsx
+++ lesson4/src/components/1-events/Event.jsx
@@ -1,7 +1,27 @@
 const Event = () => {
+  //? Event handler fonksiyon tanımlaması
+  const handleTıkla = (event) => {
+    alert("tıklandı");
+    //* event objesi üzerinden elementin özelliklerine ulaşabiliriz
+    console.log(event);
+    console.log("id", event.target.id);
+    console.log("name", event.target.name);
+  };
+
   return (
     <div>
       <h2>Events</h2>
+      <button
+        onClick={handleTıkla}
+        id="myButton"
+        name="tıkla"
+        className="btn btn-primary"
+      >
+        Tıkla
+      </button>
+      <button onClick={() => alert("Silindi")} className="btn btn-danger">
+        Sil
+      </button>
     </div>
   );
 };
