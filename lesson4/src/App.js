d1b68712b23700b1ae5a52bc9947222f1f6396c8
agpaknazli <agpaknazli19@gmail.com>
2024-08-26 19:49:52 +0200

    lesson3

---
+++ lesson4/src/App.js
@@ -0,0 +1,12 @@
+import Event from "./components/1-events/Event";
+
+function App() {
+  return (
+    <div className="App text-center">
+      <h1>Events - useState Hook</h1>
+      <Event />
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson4/src/components/1-events/Event.jsx
@@ -0,0 +1,8 @@
+const Event = () => {
+  return (
+    <div>
+      <h2>Events</h2>
+    </div>
+  );
+};
+export default Event;
