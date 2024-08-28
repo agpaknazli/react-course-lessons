fda6849b286abf351ba3aefc93cbb1dc346b32fd
agpaknazli <agpaknazli19@gmail.com>
2024-08-28 22:02:51 +0200

    keyboard.jsx

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -1,11 +1,42 @@
-import FormEvents from "./components/1-form-events/FormEvents";
-import FormObject from "./components/1-form-events/FormObject";
-function App() {
+import React from "react";
+const Keyboard = () => {
+  const handleKeyDown = (e) => {
+    // console.log(e.keyCode);
+    // console.log(e.key);
+    if (e.key === "Enter") {
+      alert("form submitted");
+    }
+    // if (e.key === "Backspace") {
+    //   alert("silmek istediğinizden eminmisiniz");
+    // }
+  };
+  const handleCopy = (e) => {
+    e.preventDefault(); //? kopyalama özelliğini engelledik
+    alert("Bu metin kopyalanamaz");
+  };
+  const handleCut = (e) => {
+    e.preventDefault(); //? kesme özelliğini engelledik
+    alert("Bu metin kesilemez");
+  };
   return (
-    <div className="App">
-      {/* <FormEvents /> */}
-      <FormObject />
+    <div className="container mt-4">
+      <h2>Keyboard Events</h2>
+      <input onKeyDown={handleKeyDown} type="text" className="form-control" />
+      <div className="mt-3">
+        <p>{}</p>
+      </div>
+      <div>
+        <textarea
+          className="form-control"
+          name="textarea"
+          id="area"
+          cols="30"
+          rows="10"
+          onCopy={handleCopy}
+          onCut={handleCut}
+        ></textarea>
+      </div>
     </div>
   );
-}
-export default App;
+};
+export default Keyboard;
--- lesson5/src/index.js
+++ lesson5/src/index.js
@@ -1,36 +1,7 @@
-import React, { useState } from "react";
-const MouseEvents = () => {
-  const [visible, setVisible] = useState(false);
-  const handleDoubleClick = (e) => e.target.classList.toggle("bg-danger");
-  return (
-    <div className="container text-center d-flex flex-column align-items-center mt-4">
-      <h2 className="text-danger">MOUSE EVENTS</h2>
-      <div
-        onMouseOver={() => setVisible(true)}
-        onMouseLeave={() => setVisible(false)}
-        id="todo-1"
-        className="bg-success text-light w-50 p-4"
-      >
-        onMouseOver & onMouseLeave
-      </div>
-      {visible && <p className="text-danger">Mouse üzerimde</p>}
-      <div
-        onDoubleClick={handleDoubleClick}
-        id="todo-2"
-        className="bg-success text-light w-50 p-4 mt-4"
-      >
-        onDoubleClick
-      </div>
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
-        <span>onMouseMove</span>
-      </div>
-      <div id="todo-4" className="bg-success text-light w-50 p-4 my-4">
-        <span>onMouseOver</span>
-      </div>
-      <div id="todo-5" className="bg-success text-light w-50 p-4 my-4">
-        <span>onMouseEnter</span>
-      </div>
-    </div>
-  );
-};
-export default MouseEvents;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
